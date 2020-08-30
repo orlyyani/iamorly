43b9f7cd18f0b085f6dd32d4f27fdf2489bfa8e3
orlyyani <orlyjohnyanson@gmail.com>
2020-08-30 19:31:14 +0800

    [UPDATE] udpate experience

--- components/Experience.vue
+++ components/Experience.vue
@@ -19,18 +19,24 @@
         <h3 class="m-top-40">Front-End Web Developer / UI/UX</h3>
         <h3>Mynd Consulting - 2 yrs 5 mos</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat..
+          Mynd Consulting was a great company. It allows me to develop my
+          Front-End and UI/UX skills. I have been into this company for almost
+          two years and five months. I started to join in 2016 as a Junior Dev
+          and I have been to different teams and learned a lot. Mynd was behind
+          the official Dr. Oz app, published by Sharecare Inc.
         </p>
         <h3 class="m-top-40">Full Stack Web Developer</h3>
         <h3>Appetiser Pty Ltd - Present</h3>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat..
+          I improved my skills by joining Appetiser Apps—one of the most leading
+          companies in Australia, founded by Michael MacRae and Jamie Shostak. I
+          started to grow as a developer since I joined this company by working
+          flexibly.
+        </p>
+        <p>
+          I joined this company in 2018 and developed several web applications
+          with different teams. Appetiser Apps have generated millions of
+          downloads and its clients are worth billions.
         </p>
       </div>
     </div>
